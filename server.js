38b7d1387b1f5a2feeef9fd91d87a093b7afdc71
NMatiukh <85105742+NMatiukh@users.noreply.github.com>
2024-10-16 03:40:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const jsonServer = require('json-server')
-const clone = require('clone')
-const data = require('./db.json')
+const jsonServer = require('json-server');
+const clone = require('clone');
+const data = require('./db.json');
+const cors = require('cors'); // додано cors
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
+// Додаємо проміжний шар для CORS
+server.use(cors());
+
 const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
     _isFake: isProductionEnv
-})
-const middlewares = jsonServer.defaults()
+});
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
 
+// Запобігаємо зміні даних у production
 server.use((req, res, next) => {
     if (req.path !== '/')
-        router.db.setState(clone(data))
-    next()
-})
+        router.db.setState(clone(data));
+    next();
+});
 
-server.use(router)
+server.use(router);
 server.listen(process.env.PORT || 8000, () => {
-    console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
